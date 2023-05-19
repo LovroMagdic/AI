3b87440aece2541edcf295cd23ff235648c80fb0
Lovro Magdic <lovro@Lovros-MacBook-Air.local>
2023-05-19 19:29:28 +0200

    lab3 update

--- lab3/solution.py
+++ lab3/solution.py
@@ -1,51 +1,78 @@
 import csv
 import math
 
-def predict(header, row, tree, effects):
-    arr = ["YES", "NO"]
-    decision = str()
+def predict(header, row, tree, result_1,result_2,result_3 ):
+    tmp = []
+    typ1 = type(tmp)
+    arr = [result_1, result_2, result_3]
+    decision = "“"
     root = tree["root"][0]
-    print(root) # weather
+    #print(root) # weather
 
     key = "root" + "_" + root # root_weather
 
     while decision not in arr:
-        for value in tree[root]:
-            if value in row:
+        for value in tree[key]:
+            typ = type(value)
+            if typ == typ1:
+                for each in value:
+                    tmp.append(each[0])
+                for value in tmp:
+                    if value in row:
+                        key = key + "_" + str(value) # root_weather_sunny
+                        root = key
+                        decision = (tree[key])[0]
+                        #print(decision)
+                    elif value in header:
+                        key = key + "_" + str(value) # root_weather_sunny
+                        root = key
+                        decision = (tree[key])[0]
+                        #print(decision)
+            elif value in row:
                 key = key + "_" + str(value) # root_weather_sunny
                 root = key
                 decision = (tree[key])[0]
-                print(decision)
+                #print(decision)
+            elif value in header:
+                key = key + "_" + str(value) # root_weather_sunny
+                root = key
+                decision = (tree[key])[0]
+                #print(decision)
 
-    print(decision)
-    return 0
+    #print(decision)
+    return decision
 
 
 def transform_data(data,parent):
+    #print("ovo je trans_data za ",parent)
     new_data = []
     for each in data:
-        if each[0] == parent:
-            each.remove(each[0])
+        if parent in each:
+            each.remove(parent)
             new_data.append(each)
     return new_data
 
-def get_yes_no(rows):
+def get_yes_no(rows, result_1, result_2, result_3):
     length = len(rows[0]) -1
-    yes , no = 0,0
-    unkown = 0
+    yes , no, unkown = 0, 0, 0
     for each in rows:
-        if each[length] == "yes":
+        if each[length] == result_1:
             yes += 1
-        elif each[length] == "no":
+        elif each[length] == result_2:
             no += 1
-        else:
+        elif each[length] == result_3:
             unkown += 1
     return yes, no, unkown
 
-def calc_entropy(yes, no):
-    if yes == 0 or no == 0:
+def calc_entropy(yes, no, unkown, result_3):
+    if (yes == 0 or no == 0 or unkown == 0) and result_3 != "":
         return 0
-    result = ((-yes/(yes+no))*math.log2(yes/(yes+no)) + (-no/(yes+no))*math.log2(no/(yes+no)))
+    elif yes == 0 or no == 0:
+        return 0
+    if result_3 != "":
+        result = ((-yes/(yes+no+unkown))*math.log2(yes/(yes+no+unkown)) + (-no/(yes+no+unkown))*math.log2(no/(yes+no+unkown)) + (-unkown/(yes+no+unkown))*math.log2(unkown/(yes+no+unkown)))
+    else:
+        result = ((-yes/(yes+no))*math.log2(yes/(yes+no)) + (-no/(yes+no))*math.log2(no/(yes+no)))
     result = round(result, 3)
     return result
 
@@ -62,32 +89,40 @@ def ID3(parent, data, parent_data, header, number, tree_node):
     yes, no , unkown = 0, 0, 0
     for key in effects:
         if key in header:
-            #print("ovo je za ", key)
+            print("ovo je za ", key)
             decision_parent = []
             for each in effects[key]:
                 for row in data:
                     if each in row:
-                        if "yes" in row:
+                        if result_1 in row:
                             yes += 1
-                        elif "no" in row:
+                        elif result_2 in row:
                             no += 1
-                        else:
+                        elif result_3 in row:
                             unkown += 1
-                #print(each, "yes >", yes, " no >", no, "unkown >", unkown)
-                if yes == 0 or no == 0:
-                    if yes > no:
-                        decision = "YES"
-                    elif no > yes:
-                        decision = "NO"
+                print(each, "yes >", yes, " no >", no, "unkown >", unkown)
+                if (yes == 0 or no == 0 or unkown == 0) and result_3 != "":
+                    if yes > no and yes > unkown:
+                        decision = result_1
+                    elif no > yes and no > unkown:
+                        decision = result_2
+                    elif unkown > no and unkown > yes:
+                        decision  = result_3
+
+                elif (yes == 0 or no == 0) and result_3 == "":
+                    if yes > no and yes > unkown:
+                        decision = result_1
+                    elif no > yes and no > unkown:
+                        decision = result_2
                 decision_parent.append([each, decision])
 
-                entropy = calc_entropy(yes, no)
+                entropy = calc_entropy(yes, no, unkown, result_3)
                 entropy_list.append(entropy)
-                ig_small += float(entropy) * float((yes+no)/number)
+                ig_small += float(entropy) * float((yes+no+unkown)/number)
                 yes, no, unkown = 0, 0, 0
-            #print(entropy_list) # lista svih izracuna entropija za cvorove
-            yes, no, unkown = get_yes_no(data)
-            ig_big = round(calc_entropy(yes, no) - (ig_small), 3)
+            print(entropy_list) # lista svih izracuna entropija za cvorove
+            yes, no, unkown = get_yes_no(data, result_1, result_2, result_3)
+            ig_big = round(calc_entropy(yes, no, unkown, result_3) - (ig_small), 3)
         
         if max[1] < ig_big:
             max[3] = decision_parent
@@ -98,47 +133,43 @@ def ID3(parent, data, parent_data, header, number, tree_node):
         if max[0] == "":
             max[3] = decision_parent
             max[2] = [0,0]
-        #print(ig_big)
+        print(ig_big)
 
         # reset za novi header
         ig_small = 0
         entropy_list = []
         yes, no, unkown = 0, 0, 0
-        #print("*****")
+        print("*****")
     #print(max[3])
     if len(tree) == 0:
         tree["root"] = [max[0]]
         tree_node = "root" + "_" + str(max[0])
-        #print(tree)
+        print(tree)
     elif len(tree) != 0:
         tree_node = tree_node + "_" + str(parent)
         tree[tree_node] = [max[0]]
-        #print(tree)
-    #ovdje je nastalo sranje jer treba skuzit kak spremat ak je grana gotova i kak spremit za kasniju predikciju
-    # tj treba izgradit stablo nekak
+        print(tree)
+    
 
-
-    if (max[2] == [0,0] or max[2] == [0,0,0]) and max[0] != "":
+    if (max[2] == [0,0] or max[2] == [0,0,0]) and max[0] != "" and parent != "NULL":
         tree_node = tree_node + "_" + str(max[0])
         tree[tree_node] = [max[3]]
+        
         for each in max[3]:
-            tree_node = tree_node + "_" + str(each[0])
-            tree[tree_node] = [each[1]]
+            #print("tu sam dodao nes")
+            tree_node_tmp = tree_node + "_" + str(each[0])
+            tree[tree_node_tmp] = [each[1]]
+        #print(tree)
         return 0
     #ovo je ako je odma odlucen yes ili no
     if max[0] == "":
         tmp = ((max[3])[0])[1]
-        tree[tree_node] = [[tmp]]
+        tree[tree_node] = [tmp]
         return 0
-    
-    #ovdje sam izabrao glavni cvor
-    
 
-
-    #print("najveci ig je >" ,max)
-    #print()
-    
-
+    #ovdje sam izabrao glavni cvo
+    print("najveci ig je >" ,max)
+    print()
     tree[tree_node] = list(effects[max[0]])
     #print(tree)
     for each in effects[max[0]]:
@@ -150,18 +181,22 @@ def ID3(parent, data, parent_data, header, number, tree_node):
             if head != max[0]:
                 new_header.append(head)
         #print()
-        #print("pregledavam efeckt ", each)
+        print("pregledavam efekt ", each)
+        #print(trans_data)
         #print()
         ID3(each, trans_data, parent_data, new_header, number, tree_node)
 
 
+
+
+#ovo je MAIN PROGRAM
 #read csv file
-file = open('volleyball.csv')
+file = open('logic_small.csv')
 csvreader = csv.reader(file)
 header = []
 rows = []
 header = next(csvreader)
-header.remove(header[-1])
+#header.remove(header[-1])
 yes, no ,unkown = 0, 0, 0
 for row in csvreader:
     rows.append(row)
@@ -173,18 +208,28 @@ for i in range(len(header)):
         effects_list.add(each[i])
     effects[header[i]] = effects_list
 
+result_1 = list(effects[header[-1]])[0]
+result_2 = list(effects[header[-1]])[1]
+result_3 = ""
+if len(effects[header[-1]]) > 2:
+    result_3 = list(effects[header[-1]])[2]
 
 
-yes, no, unkown = get_yes_no(rows)
-number = yes + no # number of actual data
-entropy = calc_entropy(yes, no)
+trash = header[-1]
+del effects[trash]
+
+
+yes, no, unkown = get_yes_no(rows, result_1, result_2, result_3)
+number = yes + no + unkown # number of actual data
+entropy = calc_entropy(yes, no, unkown, result_3)
 
 tree = dict()
 tree_node = ""
 ID3("NULL", rows, rows, header, number, tree_node)
+print(tree)
 
-
-file = open('volleyball.csv')
+# here starts prediction method
+file = open('logic_small_test.csv')
 csvreader = csv.reader(file)
 header = []
 rows = []
@@ -193,7 +238,10 @@ header.remove(header[-1])
 yes, no ,unkown = 0, 0, 0
 for row in csvreader:
     rows.append(row)
+prediction_list = []
 
 for row in rows:
-    print(row)
-    predict(header, row, tree, effects)
+    prediction = predict(header, row, tree, result_1, result_2, result_3)
+    prediction_list.append(prediction.lower())
+
+print(prediction_list)
